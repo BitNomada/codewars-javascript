228a538009c94548bb05f5ca5c57ef6afddcffff
Bit Nómada <110926262+BitNomada@users.noreply.github.com>
2022-08-23 20:28:02 +0200

    description

--- replace-with-alphabet-position.js
+++ replace-with-alphabet-position.js
@@ -1,3 +1,10 @@
+/*n this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+*/
+
 function alphabetPosition(text) {
     //Saving the alphabet into const
     const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
